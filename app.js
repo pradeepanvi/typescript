c5374efa000e50cd1ad5c22599fe9a9ae0ce8b36
pradeepanvi <pradeepkumar.anvi@gmail.com>
2020-03-22 16:50:53 +0530

    Tuples Type

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 var person = {
     name: "Max",
     age: 30,
-    hobbies: ["Sports", "Football"]
+    hobbies: ["Sports", "Football"],
+    role: [12, "author"]
 };
-var nextStep;
-nextStep = ["Ball"];
-console.log(nextStep);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var item = _a[_i];
-    console.log(item.toUpperCase());
-}
+console.log(person.role);
--- app.ts
+++ app.ts
@@ -1,14 +1,14 @@
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  //Tuples: It's a kind of array there user can add limited value in array also type will be strict.
+  role: [number, string];
+} = {
   name: "Max",
   age: 30,
-  hobbies: ["Sports", "Football"]
+  hobbies: ["Sports", "Football"],
+  role: [12, "author"]
 };
 
-let nextStep: string[];
-nextStep = ["Ball"];
-
-console.log(nextStep);
-
-for (const item of person.hobbies) {
-  console.log(item.toUpperCase());
-}
+console.log(person.role);
