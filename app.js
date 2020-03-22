82340ae16322f5b8ad5a3ce34fab5e68e713c6c1
pradeepanvi <pradeepkumar.anvi@gmail.com>
2020-03-22 18:05:30 +0530

    Literal Type

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-/*Union Type :-
-  A parameter in function can be assign with multiple type.
-  function combine(number | string, number | string){}
+/*Literal Type :-
+  It's same kind of Union Type but here need to add one extra parameter.
+  function combine(number | string, number | string, 'as-number' | 'as-text'){}
 }
 */
-function combine(input1, input2) {
+function combine(input1, input2, resultConversion) {
     var result;
-    if (typeof input1 === "number" && typeof input2 === "number") {
-        result = input1 + input2;
+    if ((typeof input1 === "number" && typeof input2 === "number") ||
+        resultConversion === "as-number") {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
     return result;
 }
-var combineAges = combine(30, 26);
+var combineAges = combine(30, 26, "as-number");
 console.log(combineAges);
-var combineNames = combine("Max", "Kumar");
+var combineNames = combine("Max", "Kumar", "as-text");
 console.log(combineNames);
--- app.ts
+++ app.ts
@@ -1,20 +1,27 @@
-/*Union Type :- 
-  A parameter in function can be assign with multiple type.
-  function combine(number | string, number | string){}
+/*Literal Type :-
+  It's same kind of Union Type but here need to add one extra parameter.
+  function combine(number | string, number | string, 'as-number' | 'as-text'){}
 }
 */
-function combine(input1: number | string, input2: number | string) {
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: "as-number" | "as-text"
+) {
   let result;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
   return result;
 }
 
-const combineAges = combine(30, 26);
+const combineAges = combine(30, 26, "as-number");
 console.log(combineAges);
 
-const combineNames = combine("Max", "Kumar");
+const combineNames = combine("Max", "Kumar", "as-text");
 console.log(combineNames);
