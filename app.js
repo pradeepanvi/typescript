09ea6edfadfa4b7a3f4f53be67682b25c9794172
pradeepanvi <pradeepkumar.anvi@gmail.com>
2020-04-05 17:47:58 +0530

    Intersection Types

--- app.js
+++ app.js
@@ -1,31 +1,18 @@
 "use strict";
-/*  Optional Parameters & Properties :
+/*  Intersection Types :
  */
-var add;
-add = function (n1, n2) {
-    return n1 + n2;
+/* This is Javascript type same as interface*/
+// type Admin = {
+//   name: string;
+//   privileges: string[];
+// };
+// type Employee = {
+//   name: string;
+//   startDate: Date;
+// };
+// type ElevatedEmployee = Admin & Employee;
+var e1 = {
+    name: "Max",
+    privileges: ["create-server"],
+    startDate: new Date(),
 };
-var Person = /** @class */ (function () {
-    function Person(n) {
-        this.age = 30;
-        if (n) {
-            this.name = n;
-        }
-    }
-    Person.prototype.greet = function (pharse) {
-        if (this.name) {
-            console.log(pharse + " " + this.name);
-        }
-        else {
-            console.log("Hi!");
-        }
-    };
-    return Person;
-}());
-var user1;
-user1 = new Person("Max");
-// user1.name = 'Sam'; // you can't add this now
-user1.greet("Hi there I am");
-console.log(user1);
-// Hi there I am Max
-// app.js: 18 Person { age: 30, name: "Max" }
--- app.ts
+++ app.ts
@@ -1,51 +1,33 @@
-/*  Optional Parameters & Properties :
+/*  Intersection Types :
  */
 
-// type AddFn = (a: number, b: number) => number;
-interface AddFn {
-  (a: number, b: number): number;
+interface Admin {
+  name: string;
+  privileges: string[];
 }
 
-let add: AddFn;
+interface Employee {
+  name: string;
+  startDate: Date;
+}
 
-add = (n1: number, n2: number) => {
-  return n1 + n2;
+interface ElevatedEmployee extends Admin, Employee {}
+
+/* This is Javascript type same as interface*/
+// type Admin = {
+//   name: string;
+//   privileges: string[];
+// };
+
+// type Employee = {
+//   name: string;
+//   startDate: Date;
+// };
+
+// type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+  name: "Max",
+  privileges: ["create-server"],
+  startDate: new Date(),
 };
-
-interface Named {
-  readonly name?: string;
-  outputName?: string;
-}
-
-interface Greetable extends Named {
-  greet(pharse: string): void;
-}
-
-class Person implements Greetable {
-  name?: string;
-  age = 30;
-
-  constructor(n: string) {
-    if (n) {
-      this.name = n;
-    }
-  }
-
-  greet(pharse: string) {
-    if (this.name) {
-      console.log(pharse + " " + this.name);
-    } else {
-      console.log("Hi!");
-    }
-  }
-}
-
-let user1: Greetable;
-
-user1 = new Person("Max");
-// user1.name = 'Sam'; // you can't add this now
-
-user1.greet("Hi there I am");
-console.log(user1);
-// Hi there I am Max
-// app.js: 18 Person { age: 30, name: "Max" }
