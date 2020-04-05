98d453ff0de9b7391e54d8cbd95c0252eb775c14
pradeepanvi <pradeepkumar.anvi@gmail.com>
2020-04-05 16:35:00 +0530

    A First Interface

--- app.js
+++ app.js
@@ -1,92 +1,13 @@
 "use strict";
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-/*  Singletons & Private Constructors :
-    Singletons is a design pattern for use inside of a function
-    Private Constructors that can be no longer use to constructor outside of a class
- */
-var Department = /** @class */ (function () {
-    //here we have to defined public as well, because it won't take
-    function Department(id, name) {
-        this.id = id;
-        this.name = name;
-        this.employees = [];
-    }
-    Department.createEmployee = function (name) {
-        return { name: name };
-    };
-    Department.prototype.addEmployee = function (employee) {
-        // validation etc
-        // this.id = '3'  user will not able to add any value here
-        this.employees.push(employee);
-    };
-    Department.prototype.printEmployeeInformation = function () {
-        console.log(this.employees.length);
-        console.log(this.employees);
-    };
-    Department.firstYear = 2020;
-    return Department;
-}());
-var AccountingDepartment = /** @class */ (function (_super) {
-    __extends(AccountingDepartment, _super);
-    function AccountingDepartment(id, reports) {
-        var _this = _super.call(this, id, "Accounting") || this;
-        _this.reports = reports;
-        _this.lastReport = reports[0];
-        return _this;
-    }
-    Object.defineProperty(AccountingDepartment.prototype, "mostRecentReport", {
-        //Getters
-        get: function () {
-            if (this.lastReport) {
-                return this.lastReport;
-            }
-            throw new Error("No Report found");
-        },
-        //Setters
-        set: function (value) {
-            if (!value) {
-                throw new Error("Please pass in a valid value");
-            }
-            this.addReport(value);
-        },
-        enumerable: true,
-        configurable: true
-    });
-    AccountingDepartment.getInstance = function () {
-        if (AccountingDepartment.instance) {
-            return this.instance;
-        }
-        this.instance = new AccountingDepartment("d2", []);
-        return this.instance;
-    };
-    AccountingDepartment.prototype.describe = function () {
-        console.log("Accounting Department - ID: " + this.id);
-    };
-    AccountingDepartment.prototype.addEmployee = function (name) {
-        if (name === "Max") {
-            return;
-        }
-        this.employees.push(name);
-    };
-    AccountingDepartment.prototype.addReport = function (text) {
-        this.reports.push(text);
-        this.lastReport = text;
-    };
-    return AccountingDepartment;
-}(Department));
-var employee1 = Department.createEmployee("Max");
-console.log(employee1, Department.firstYear);
-var accounting = AccountingDepartment.getInstance();
-console.log(accounting);
+/*  A First Interface :
+    Interface just a structor for use javascript objects
+*/
+var user1;
+user1 = {
+    name: "Max",
+    age: 30,
+    greet: function (pharse) {
+        console.log(pharse + " " + this.name);
+    },
+};
+user1.greet("Hi there I am");
--- app.ts
+++ app.ts
@@ -1,86 +1,22 @@
-/*  Singletons & Private Constructors :
-    Singletons is a design pattern for use inside of a function
-    Private Constructors that can be no longer use to constructor outside of a class
- */
-abstract class Department {
-  static firstYear = 2020;
+/*  A First Interface :
+    Interface just a structor for use javascript objects
+*/
 
-  protected employees: string[] = [];
+interface Person {
+  name: string;
+  age: number;
 
-  //here we have to defined public as well, because it won't take
-  constructor(protected readonly id: string, public name: string) {}
-
-  static createEmployee(name: string) {
-    return { name: name };
-  }
-
-  abstract describe(this: Department): void;
-
-  addEmployee(employee: string) {
-    // validation etc
-    // this.id = '3'  user will not able to add any value here
-    this.employees.push(employee);
-  }
-
-  printEmployeeInformation() {
-    console.log(this.employees.length);
-    console.log(this.employees);
-  }
+  greet(pharse: string): void;
 }
 
-class AccountingDepartment extends Department {
-  private lastReport: string;
-  private static instance: AccountingDepartment;
+let user1: Person;
 
-  //Getters
-  get mostRecentReport() {
-    if (this.lastReport) {
-      return this.lastReport;
-    }
-    throw new Error("No Report found");
-  }
+user1 = {
+  name: "Max",
+  age: 30,
+  greet(pharse: string) {
+    console.log(pharse + " " + this.name);
+  },
+};
 
-  //Setters
-  set mostRecentReport(value: string) {
-    if (!value) {
-      throw new Error("Please pass in a valid value");
-    }
-    this.addReport(value);
-  }
-
-  private constructor(id: string, private reports: string[]) {
-    super(id, "Accounting");
-    this.lastReport = reports[0];
-  }
-
-  static getInstance() {
-    if (AccountingDepartment.instance) {
-      return this.instance;
-    }
-    this.instance = new AccountingDepartment("d2", []);
-    return this.instance;
-  }
-
-  describe() {
-    console.log("Accounting Department - ID: " + this.id);
-  }
-
-  addEmployee(name: string) {
-    if (name === "Max") {
-      return;
-    }
-    this.employees.push(name);
-  }
-
-  addReport(text: string) {
-    this.reports.push(text);
-    this.lastReport = text;
-  }
-}
-
-const employee1 = Department.createEmployee("Max");
-console.log(employee1, Department.firstYear);
-
-const accounting = AccountingDepartment.getInstance();
-
-console.log(accounting);
+user1.greet("Hi there I am");
