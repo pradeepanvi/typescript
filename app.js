34fd7067cb7207f8f15d06e0e1666660faa68fd4
pradeepanvi <pradeepkumar.anvi@gmail.com>
2020-04-10 15:33:34 +0530

    Built-in Generics & What are Generics?

--- app.js
+++ app.js
@@ -1,24 +1,12 @@
 "use strict";
-/*  Nullish Coalescing :
-    userInput ?? "DEFAULT" If userInput value will be undefined or null than the DEFAULT value will show.
+/*  Built-in Generics & What are Generics? :
+    Whenever you see Array or Promise <any>
  */
-var _a;
-// If we will not user above function than split will not work
-function add(a, b) {
-    if (typeof a === "string" || typeof b === "string") {
-        return a.toString() + b.toString();
-    }
-    return a + b;
-}
-var result = add("Max", "Same");
-result.split(" ");
-var fetchedUserData = {
-    id: "u1",
-    name: "Max",
-    job: { title: "CEO" },
-};
-console.log((_a = fetchedUserData === null || fetchedUserData === void 0 ? void 0 : fetchedUserData.job) === null || _a === void 0 ? void 0 : _a.title);
-// const userInput = "";
-var userInput = undefined;
-var storedData = userInput !== null && userInput !== void 0 ? userInput : "DEFAULT";
-console.log(storedData);
+var names = []; // string []
+names[0].split(" ");
+var promise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve(10);
+    }, 2000);
+});
+promise.then(function (data) { });
--- app.ts
+++ app.ts
@@ -1,34 +1,14 @@
-/*  Nullish Coalescing :
-    userInput ?? "DEFAULT" If userInput value will be undefined or null than the DEFAULT value will show.
+/*  Built-in Generics & What are Generics? :
+    Whenever you see Array or Promise <any>
  */
 
-type Combinable = string | number;
+const names: Array<string> = []; // string []
+names[0].split(" ");
 
-function add(a: number, b: number): number;
-function add(a: string, b: string): string;
-function add(a: string, b: number): string;
-function add(a: number, b: string): string;
-// If we will not user above function than split will not work
-function add(a: Combinable, b: Combinable) {
-  if (typeof a === "string" || typeof b === "string") {
-    return a.toString() + b.toString();
-  }
-  return a + b;
-}
+const promise: Promise<number> = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(10);
+  }, 2000);
+});
 
-const result = add("Max", "Same");
-result.split(" ");
-
-const fetchedUserData = {
-  id: "u1",
-  name: "Max",
-  job: { title: "CEO" },
-};
-
-console.log(fetchedUserData?.job?.title);
-
-// const userInput = "";
-const userInput = undefined;
-const storedData = userInput ?? "DEFAULT";
-
-console.log(storedData);
+promise.then((data) => {});
