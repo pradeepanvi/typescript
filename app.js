b00583db1266b987163ee713c93db30fc465f21f
pradeepanvi <pradeepkumar.anvi@gmail.com>
2020-04-10 14:15:36 +0530

    Type Casting

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
 "use strict";
-/*  Discriminated Unions :
+/*  Type Casting :
  */
-function moveAnimation(animal) {
-    var speed;
-    switch (animal.type) {
-        case "bird":
-            speed = animal.flyingSpeed;
-            break;
-        case "horse":
-            speed = animal.flyingSpeed;
-    }
-    console.log("Moving at Speed: " + speed);
-}
-moveAnimation({ type: "bird", flyingSpeed: 10 });
+// const userInputElement = <HTMLInputElement>(
+//   document.getElementById("user-input")!
+// );
+var userInputElement = document.getElementById("user-input");
+userInputElement.value = "Hi there!";
--- app.ts
+++ app.ts
@@ -1,28 +1,10 @@
-/*  Discriminated Unions :
+/*  Type Casting :
  */
 
-interface Bird {
-  type: "bird";
-  flyingSpeed: number;
-}
-
-interface Horse {
-  type: "horse";
-  flyingSpeed: number;
-}
-
-type Animal = Bird | Horse;
-
-function moveAnimation(animal: Animal) {
-  let speed;
-  switch (animal.type) {
-    case "bird":
-      speed = animal.flyingSpeed;
-      break;
-    case "horse":
-      speed = animal.flyingSpeed;
-  }
-  console.log("Moving at Speed: " + speed);
-}
-
-moveAnimation({ type: "bird", flyingSpeed: 10 });
+// const userInputElement = <HTMLInputElement>(
+//   document.getElementById("user-input")!
+// );
+const userInputElement = document.getElementById(
+  "user-input"
+)! as HTMLInputElement;
+userInputElement.value = "Hi there!";
