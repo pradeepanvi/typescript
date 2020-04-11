3840ebb023a4d4b72ccb2a6f54f479130b9ae61c
pradeepanvi <pradeepkumar.anvi@gmail.com>
2020-04-11 16:48:15 +0530

    Returning and changing a Class in a

--- app.js
+++ app.js
@@ -1,81 +1,65 @@
 "use strict";
-/*  07 Accessor & Parameter Decorators :
+/*  09 Returning and changing a Class in a :
 
  */
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __param = (this && this.__param) || function (paramIndex, decorator) {
-    return function (target, key) { decorator(target, key, paramIndex); }
-};
-function Log(target, propertyName) {
-    console.log('Property decorator!');
-    console.log(target, propertyName);
-}
-function Log2(target, name, descriptor) {
-    console.log('Accessor decorator!');
-    console.log(target);
-    console.log(name);
-    console.log(descriptor);
-}
-function Log3(target, name, descriptor) {
-    console.log('Method decorator!');
-    console.log(target);
-    console.log(name);
-    console.log(descriptor);
-}
-function Log4(target, name, position) {
-    console.log('Parameter decorator!');
-    console.log(target);
-    console.log(name);
-    console.log(position);
-}
-var Product = /** @class */ (function () {
-    function Product(t, p) {
-        this.title = t;
-        this._price = p;
-    }
-    Object.defineProperty(Product.prototype, "price", {
-        set: function (val) {
-            if (val > 0) {
-                this._price = val;
-            }
-            else {
-                throw new Error('Invalid price - should be positive!');
-            }
-        },
-        enumerable: true,
-        configurable: true
-    });
-    Product.prototype.getPriceWithTax = function (tax) {
-        return this.price * (1 + tax);
+function Logger(logString) {
+    return function (constructor) {
+        console.log(logString);
+        console.log(constructor);
     };
-    __decorate([
-        Log
-    ], Product.prototype, "title", void 0);
-    __decorate([
-        Log2
-    ], Product.prototype, "price", null);
-    __decorate([
-        Log3,
-        __param(0, Log4)
-    ], Product.prototype, "getPriceWithTax", null);
-    return Product;
+}
+function WithTemplate(template, hookId) {
+    return function (originalConstructor) {
+        return /** @class */ (function (_super) {
+            __extends(class_1, _super);
+            function class_1() {
+                var _ = [];
+                for (var _i = 0; _i < arguments.length; _i++) {
+                    _[_i] = arguments[_i];
+                }
+                var _this = _super.call(this) || this;
+                console.log('Rendering template');
+                var hookEl = document.getElementById(hookId);
+                var p = new originalConstructor();
+                if (hookEl) {
+                    hookEl.innerHTML = template;
+                    hookEl.querySelector('h1').textContent = _this.name;
+                }
+                return _this;
+            }
+            return class_1;
+        }(originalConstructor));
+    };
+}
+var Person = /** @class */ (function () {
+    function Person() {
+        this.name = 'Max';
+        console.log('Creating person object...');
+    }
+    Person = __decorate([
+        Logger('LOGGING - PERSON'),
+        WithTemplate('<h1>My Person Object</h1>', 'app')
+    ], Person);
+    return Person;
 }());
-// Property decorator!
-// app.js: 16 { getPriceWithTax: ƒ, constructor: ƒ } "title"
-// app.js: 19 Accessor decorator!
-// app.js: 20 { getPriceWithTax: ƒ, constructor: ƒ }
-// app.js: 21 price
-// app.js: 22 { get: undefined, enumerable: true, configurable: true, set: ƒ }
-// app.js: 31 Parameter decorator!
-// app.js: 32 { getPriceWithTax: ƒ, constructor: ƒ }
-// app.js: 33 getPriceWithTax
-// app.js: 34 0
-// app.js: 25 Method decorator!
-// app.js: 26 { getPriceWithTax: ƒ, constructor: ƒ }
-// app.js: 27 getPriceWithTax
-// app.js: 28 { writable: true, enumerable: true, configurable: true, value: ƒ }
+var pers = new Person();
+console.log(pers);
--- app.ts
+++ app.ts
@@ -1,69 +1,40 @@
-/*  07 Accessor & Parameter Decorators :
+/*  09 Returning and changing a Class in a :
 
  */
 
-function Log(target: any, propertyName: string | Symbol) {
-  console.log('Property decorator!');
-  console.log(target, propertyName);
+function Logger(logString: string) {
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  }
 }
 
-function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
-  console.log('Accessor decorator!');
-  console.log(target);
-  console.log(name);
-  console.log(descriptor);
-}
-
-function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor) {
-  console.log('Method decorator!');
-  console.log(target);
-  console.log(name);
-  console.log(descriptor);
-}
-
-function Log4(target: any, name: string | Symbol, position: number) {
-  console.log('Parameter decorator!');
-  console.log(target);
-  console.log(name);
-  console.log(position);
-}
-
-class Product {
-  @Log
-  title: string;
-  private _price: number;
-
-  @Log2
-  set price(val: number) {
-    if (val > 0) {
-      this._price = val;
-    } else {
-      throw new Error('Invalid price - should be positive!');
+function WithTemplate(template: string, hookId: string) {
+  return function <T extends { new(...args: any[]): { name: string } }>(originalConstructor: T) {
+    return class extends originalConstructor {
+      constructor(..._: any[]) {
+        super();
+        console.log('Rendering template')
+        const hookEl = document.getElementById(hookId);
+        const p = new originalConstructor();
+        if (hookEl) {
+          hookEl.innerHTML = template;
+          hookEl.querySelector('h1')!.textContent = this.name;
+        }
+      }
     }
   }
+}
 
-  constructor(t: string, p: number) {
-    this.title = t;
-    this._price = p;
-  }
+@Logger('LOGGING - PERSON')
+@WithTemplate('<h1>My Person Object</h1>', 'app')
 
-  @Log3
-  getPriceWithTax(@Log4 tax: number) {
-    return this.price * (1 + tax);
+class Person {
+  name = 'Max';
+  constructor() {
+    console.log('Creating person object...');
   }
 }
 
-// Property decorator!
-// app.js: 16 { getPriceWithTax: ƒ, constructor: ƒ } "title"
-// app.js: 19 Accessor decorator!
-// app.js: 20 { getPriceWithTax: ƒ, constructor: ƒ }
-// app.js: 21 price
-// app.js: 22 { get: undefined, enumerable: true, configurable: true, set: ƒ }
-// app.js: 31 Parameter decorator!
-// app.js: 32 { getPriceWithTax: ƒ, constructor: ƒ }
-// app.js: 33 getPriceWithTax
-// app.js: 34 0
-// app.js: 25 Method decorator!
-// app.js: 26 { getPriceWithTax: ƒ, constructor: ƒ }
-// app.js: 27 getPriceWithTax
-// app.js: 28 { writable: true, enumerable: true, configurable: true, value: ƒ }
+const pers = new Person();
+console.log(pers);
