d3b62f7627f5b7c5531c050adfa71a59e1cb5374
pradeepanvi <pradeepkumar.anvi@gmail.com>
2020-04-11 13:32:12 +0530

    The -keyof- Constraint

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
 "use strict";
-/*  Another Generic Function :
+/*  06 The -keyof- Constraint :
  */
-function countAndDescribe(elements) {
-    var descriptionText = "Got no value.";
-    if (elements.length === 1) {
-        descriptionText = "Got 1 element.";
-    }
-    else if (elements.length > 1) {
-        descriptionText = 'Got ' + elements.length + ' elements.';
-    }
-    return [elements, descriptionText];
+function extractAndConvert(obj, key) {
+    return 'Value: ' + obj[key];
 }
-console.log(countAndDescribe('Hi there!'));
-// Array(2)
-// 0: "Hi there!"
-// 1: "Got 9 elements."
-// length: 2
+extractAndConvert({ name: 'Max' }, 'name');
+// "Value: Max"
--- app.ts
+++ app.ts
@@ -1,22 +1,8 @@
-/*  Another Generic Function :
+/*  06 The -keyof- Constraint :
  */
 
-interface Lengthy {
-  length: number;
+function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
+  return 'Value: ' + obj[key];
 }
-
-function countAndDescribe<T extends Lengthy>(elements: T) {
-  let descriptionText = "Got no value.";
-  if (elements.length === 1) {
-    descriptionText = "Got 1 element.";
-  } else if (elements.length > 1) {
-    descriptionText = 'Got ' + elements.length + ' elements.'
-  }
-  return [elements, descriptionText];
-}
-
-console.log(countAndDescribe('Hi there!'));
-// Array(2)
-// 0: "Hi there!"
-// 1: "Got 9 elements."
-// length: 2
+extractAndConvert({ name: 'Max' }, 'name');
+// "Value: Max"
